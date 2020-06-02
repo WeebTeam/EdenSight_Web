99104eb624dd2e3b2bf5f59f038cba3d66d4b8e7
chrislow8683 <kururu1999@hotmail.com>
2020-06-03 01:32:25 +0800

    Added some basic functions to staff and resident page
    Basic functions added
    Staff page - transferring data from staff to resident page
    Resident page - Dashboard showing basic data from dummy data, history having show more button that shows a modal

--- src/app/resident/resident.component.ts
+++ src/app/resident/resident.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-resident',
@@ -6,10 +9,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./resident.component.css']
 })
 export class ResidentComponent implements OnInit {
+  resident;
+  vitalSigns;
+  vs1;
+  vs2;
+  vs3;
+  vs4;
+  date;
+  closeResult: string;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router:Router, private activatedRoute:ActivatedRoute, private modalService: NgbModal) {
+    console.log(this.router.getCurrentNavigation().extras.state);
   }
 
+  ngOnInit(): void {
+  	this.resident = history.state.data;
+  	this.vitalSigns = this.resident.vitalSigns;
+  }
+
+  public ShowMore(event, vs, content){
+  	this.vs1 = vs.vs1;
+  	this.vs2 = vs.vs2;
+  	this.vs3 = vs.vs3;
+  	this.vs4 = vs.vs4;
+  	this.date = vs.date;
+  	console.log(vs);
+
+  	this.modalService.open(content, { centered: true });
+  }
 }
--- src/app/staff/staff.component.ts
+++ src/app/staff/staff.component.ts
@@ -10,12 +10,19 @@ export class StaffComponent implements OnInit {
   searchResident = "";
   selectedRes = "";
 
-  residents: {id: string, name: string, status: string, caretaker: string, room: string, pNum: string }[] = [
-  	{id: '1', name: "loli", status: "Healthy", caretaker: "Louis", room: "R301", pNum: "011202016243"},
-  	{id: '2', name: "pop", status: "Healthy", caretaker: "Angel", room: "R302",  pNum: "011202011113"},
-  	{id: '3', name: "aa", status: "Not Healthy", caretaker: "Papi", room: "R303",  pNum: "01233333232"},
-  	{id: '4', name: "ddd", status: "Healthy", caretaker: "Irwin", room: "R304",  pNum: "01111123233"},
-  	{id: '5', name: "ggg", status: "Not Healthy", caretaker: "Oppi", room: "R305",  pNum: "01782829929"}
+  residents: {id: string, name: string, status: string, caretaker: string, room: string, pNum: string, 
+    vitalSigns: Array<{vs1: string, vs2: string, vs3: string, vs4: string, date: string}>}[] = [
+  	{id: '1', name: "loli", status: "Healthy", caretaker: "Louis", room: "R301", pNum: "011202016243", 
+        vitalSigns: [{vs1: "20", vs2: "21", vs3: "22", vs4: "23", date: "23/02/2020"}, 
+                     {vs1: "19", vs2: "15", vs3: "47", vs4: "34", date: "02/03/2020"}]},
+  	{id: '2', name: "pop", status: "Healthy", caretaker: "Angel", room: "R302",  pNum: "011202011113", 
+        vitalSigns: [{vs1: "20", vs2: "21", vs3: "22", vs4: "23", date: "23/02/2020"}]},
+  	{id: '3', name: "aa", status: "Not Healthy", caretaker: "Papi", room: "R303",  pNum: "01233333232", 
+        vitalSigns: [{vs1: "20", vs2: "21", vs3: "22", vs4: "23", date: "23/02/2020"}]},
+  	{id: '4', name: "ddd", status: "Healthy", caretaker: "Irwin", room: "R304",  pNum: "01111123233", 
+        vitalSigns: [{vs1: "20", vs2: "21", vs3: "22", vs4: "23", date: "23/02/2020"}]},
+  	{id: '5', name: "ggg", status: "Not Healthy", caretaker: "Oppi", room: "R305",  pNum: "01782829929", 
+        vitalSigns: [{vs1: "20", vs2: "21", vs3: "22", vs4: "23", date: "23/02/2020"}]}
   ];
 
   constructor() { }
